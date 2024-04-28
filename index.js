379bb371b57edd9b97188b2ca7aad8c2882b9a7b
Meghla15 <meherunnesa1505@gmail.com>
2024-04-28 15:00:52 +0600

    delete added

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/delete/:id', async(req,res) =>{
+      const result = await addedSpotCollection.deleteOne({_id : new ObjectId(req.params.id)})
+      console.log(result)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
